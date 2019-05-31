c05167c480e7f8684695d16bd3997da1e9328e91
Holly Wardlaw <hollywardlaw@matts-mbp.home>
2019-05-31 17:15:19 +0100

    completed layout and styling for task adder

--- blue-prism/src/components/Task.jsx
+++ blue-prism/src/components/Task.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { FiMinusCircle, FiPlusCircle } from "react-icons/fi";
+import { TiStarOutline } from "react-icons/ti";
+import { IoIosSettings } from "react-icons/io";
 
 
 const Task = props => {
@@ -11,6 +14,20 @@ const Task = props => {
           <input className="text-input"
             placeholder="Process description" required></input>
         </form>
+        <div className="controls">
+          <p className="workers-assigned">Workers assigned</p>
+          <FiMinusCircle className="minus" />
+          <p className="count">0 / 25</p>
+          <FiPlusCircle className="plus" />
+          <p className="rating"><TiStarOutline className="star" /><TiStarOutline className="star" /><TiStarOutline className="star" /></p>
+          <div className="bottom-bar">
+
+            <IoIosSettings className="settings" />
+            <p className="unassigned"
+            >Unassigned</p>
+          </div>
+
+        </div>
       </section>
     </main>
   );
