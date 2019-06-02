24c155493f00071d8a7438505f8bd520e94d95a4
Holly Wardlaw <hollywardlaw@matts-mbp.home>
2019-06-02 17:39:51 +0100

    Added functionality to increment or decrement workers assigned with plus/minus buttons

--- blue-prism/src/components/Controls.jsx
+++ blue-prism/src/components/Controls.jsx
@@ -1,25 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { FiMinusCircle, FiPlusCircle } from "react-icons/fi";
 import { TiStarOutline } from "react-icons/ti";
 import { IoIosSettings } from "react-icons/io";
 import '../css/Controls.css'
 
-const Controls = props => {
-  return (
-    <div className="controls">
-      <p className="workers-assigned">Workers assigned</p>
-      <FiMinusCircle className="minus" />
-      <p className="count">0 / 25</p>
-      <FiPlusCircle className="plus" />
-      <p className="rating"><TiStarOutline className="star" /><TiStarOutline className="star" /><TiStarOutline className="star" /></p>
-      <div className="bottom-bar">
+class Controls extends Component {
+  state = {
+    workers: 0
+  }
+  render() {
+    return (
+      <div className="controls">
+        <p className="workers-assigned">Workers assigned</p>
+        <FiMinusCircle className="minus" onClick={event => this.handleClick('workers', -1)} />
+        <p className="count">{this.state.workers} / 25</p>
+        <FiPlusCircle className="plus" onClick={event => this.handleClick('workers', 1)} />
+        <p className="rating"><TiStarOutline className="star" /><TiStarOutline className="star" /><TiStarOutline className="star" /></p>
+        <div className="bottom-bar">
 
-        <IoIosSettings className="settings" />
-        <p className="unassigned"
-        >Unassigned</p>
+          <IoIosSettings className="settings" />
+          {this.state.workers > 0 ? <p className="assigned-unassigned"
+          >Assigned</p> : <p className="assigned-unassigned"
+          >Unassigned</p>}
+
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
+
+  handleClick = (key, value) => {
+    const { workers } = this.state
+    if (workers + value >= 0 && workers + value <= 25) {
+      this.setState({ [key]: workers + value })
+    }
+  }
+
 };
 
 export default Controls;
--- blue-prism/src/components/Task.jsx
+++ blue-prism/src/components/Task.jsx
@@ -7,16 +7,25 @@ import '../css/Task.css';
 
 class Task extends Component {
   state = {
-    play: false
+    play: false,
+    inspect: false
   };
   render() {
+
     return (
       <div>
         <section className="task">
           <Form />
-          {this.state.play && <div className="inspect"> <IoMdEye className="eye-logo" /> <div className="progress-bar"> <span style={{ width: "75%" }}></span></div></div>}
+          {this.state.play && !this.state.inspect && <div className="inspect"> <IoMdEye className="eye-logo" onClick={event => this.handleClick('inspect', !this.state.inspect)} /> <div className="progress-bar"> <span style={{ width: "75%" }}></span></div></div>}
 
-          {!this.state.play ? <IoIosPlay className="play" onClick={this.handleClick} /> : <IoIosPause className="pause" onClick={this.handleClick} />}
+          {this.state.play && this.state.inspect && <div className="inspect"> <IoMdEye className="eye-logo" /> <div className="gradient-bar"> <ul className="list">
+            <li>Tasks in queue: <b>6</b></li>
+            <li>Average task time: <b>12s</b></li><br></br>
+            <li>Tasks completed: <b>18</b></li>
+            <li>Time remaining: <b>01:12</b></li>
+          </ul></div></div>}
+
+          {!this.state.play ? <IoIosPlay className="play" onClick={event => this.handleClick('play', !this.state.play)} /> : <IoIosPause className="pause" onClick={event => this.handleClick('play', !this.state.play)} />}
 
           <Controls />
 
@@ -24,8 +33,8 @@ class Task extends Component {
       </div>
     );
   };
-  handleClick = e => {
-    this.setState({ play: !this.state.play })
+  handleClick = (key, value) => {
+    this.setState({ [key]: value })
   }
 }
 
