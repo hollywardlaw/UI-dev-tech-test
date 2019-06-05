2c3c2eebe297764518a78d2bf2ed8deea9d520e6
Holly Wardlaw <hollywardlaw@matts-mbp.home>
2019-06-05 16:29:53 +0100

    added a read me

--- blue-prism/src/App.test.js
+++ blue-prism/src/App.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow, mount } from 'enzyme';
+import { shallow } from 'enzyme';
 import App from './App';
 import Header from './components/Header.jsx';
 import Tasks from './components/Tasks';
